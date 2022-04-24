16b1531a26d3e2f2d825adbaae647bbc4136429d
dev-kimdo <dev.kimdo@gmail.com>
2022-04-24 13:33:54 +0900

    Wetube Templates - Mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,35 @@
-import res from "express/lib/response";
 
-export const trending = (req,res) => res.render("home", {pageTitle: "Home"});
-export const see = (req, res) => res.render("watch");
-export const edit = (req,res) => res.render("edit");
+export const trending = (req,res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views:59,
+            id:1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views:59,
+            id:1,
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views:59,
+            id:1,
+        },
+    ];
+    res.render("home", {pageTitle: "Home", videos});
+};
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
+export const edit = (req,res) => res.render("edit", {pageTitle: "Edit"});
 export const search = (req, res) => res.send("Search");
 export const upload = (req,res) => res.send("Upload");
 export const deleteVideo = (req,res) => {
