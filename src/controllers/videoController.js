bc6bf8097ac5aa75f6359c6fd1c5a79498fb3d0b
dev-kimdo <dev.kimdo@gmail.com>
2022-04-26 11:47:48 +0900

    Wetube MONGODB & MONGOOSE - Array Database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,38 @@
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views:59,
+        id:1,
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views:59,
+        id:2,
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views:59,
+        id:3,
+    },
+];
 
 export const trending = (req,res) => {
-    const videos = [
-        {
-            title: "First Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views:59,
-            id:1,
-        },
-        {
-            title: "Second Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views:59,
-            id:1,
-        },
-        {
-            title: "Third Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views:59,
-            id:1,
-        },
-    ];
-    res.render("home", {pageTitle: "Home", videos});
+    return res.render("home", {pageTitle: "Home", videos});
 };
-export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
+export const see = (req, res) => {
+    const { id } = req.params;  //const id = req.params.id; 와 동일함 (현재 작성된 것은 ES6를 사용한 거임)
+    const video = videos[id-1];
+    return res.render("watch", {pageTitle: `Watching ${video.title}`});
+};
 export const edit = (req,res) => res.render("edit", {pageTitle: "Edit"});
 export const search = (req, res) => res.send("Search");
 export const upload = (req,res) => res.send("Upload");
