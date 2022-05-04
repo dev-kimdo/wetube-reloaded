66f1fe08cd35e7e790f21f18702d73a443bbe459
dev-kimdo <dev.kimdo@gmail.com>
2022-05-04 14:28:06 +0900

    Wetube MONGODB & MONGOOSE - promise

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,23 @@
 import Video from "../models/Video";
 
-export const home = (req,res) => {
-    console.log("Start");
-    Video.find({},(error, videos) => {
-        console.log("Finished");
+// export const home = (req,res) => {
+//     Video.find({},(error, videos) => {
+//     if(error) {
+//         return res.render("server-error");
+//     }
+//     return res.render("home", {pageTitle: "Home", videos});
+// });
+// };
+
+export const home = async(req,res) => {
+    try {
+        const videos = await Video.find({});
         return res.render("home", {pageTitle: "Home", videos});
-    });
+    } catch {
+        return res.render("server-error");
+    }
 };
+
 export const watch = (req, res) => {
     const { id } = req.params;  //const id = req.params.id; 와 동일함 (현재 작성된 것은 ES6를 사용한 거임)
     return res.render("watch", {pageTitle: `Watching`});
